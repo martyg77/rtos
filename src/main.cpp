a3be25b43844e9b5440e1ce05254628a481df1bd
Martin Gallant <martyg@goodbit.net>
2021-04-16 07:28:10 -0700

    Cosmetic

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@ const gpio_num_t GPIO_LED_RED = GPIO_NUM_0;
 const gpio_num_t GPIO_LED_GREEN = GPIO_NUM_2;
 const gpio_num_t GPIO_LED_BLUE = GPIO_NUM_4;
 
-extern "C" { void app_main(); }
-
 Flasher red({GPIO_LED_RED, 250});
 Flasher green({GPIO_LED_GREEN, 300});
 Flasher blue({GPIO_LED_BLUE, 500});
+
+extern "C" { void app_main(); }
 
 void app_main() {}
