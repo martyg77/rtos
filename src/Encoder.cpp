8fa304ee5111f51a9c281da40c0404e9855bce67
Martin Gallant <martyg@goodbit.net>
2021-04-19 16:58:24 -0700

    Encoder: Implementation functional

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/queue.h>
 #include <driver/gpio.h>
 #include "Encoder.h"
-
-void encoderGpioHandler(void* p) {
-    TaskHandle_t* t = (TaskHandle_t*) p;
-    BaseType_t pxHigherPriorityTaskWoken = pdFALSE; // Not sure what this does
-    vTaskNotifyGiveFromISR(*t, &pxHigherPriorityTaskWoken);
-    portYIELD_FROM_ISR(); // Not sure what this does
-}
 
 typedef struct {
     gpio_num_t pinA;
     gpio_num_t pinB;
+    QueueHandle_t queue;
+    int16_t value;
 } encoderProcess_t;
+
+void encoderGpioHandler(void* p) {
+    encoderProcess_t* tcb = (encoderProcess_t*) p;
+    uint8_t ab = (gpio_get_level(tcb->pinA) << 1) | gpio_get_level(tcb->pinB);
+    xQueueSendFromISR(tcb->queue, &ab, NULL);
+}
 
 void encoderProcess(void* p) {
     encoderProcess_t* tcb = (encoderProcess_t*) p;
@@ -30,18 +32,22 @@ void encoderProcess(void* p) {
         .intr_type = GPIO_INTR_NEGEDGE};        
     gpio_config(&g);
 
-    TaskHandle_t task = xTaskGetCurrentTaskHandle();
-    gpio_isr_handler_add(tcb->pinA, encoderGpioHandler, &task);
-    gpio_isr_handler_add(tcb->pinB, encoderGpioHandler, &task);
+    tcb->queue = xQueueCreate(16, sizeof(uint8_t));
+    gpio_isr_handler_add(tcb->pinA, encoderGpioHandler, tcb);
+    gpio_isr_handler_add(tcb->pinB, encoderGpioHandler, tcb);
 
-    // Main loop
-    uint32_t rc;
+    uint8_t state = 0;
     while (true) {
-        rc = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        printf("(%i) Encoder %i\n", xTaskGetTickCount(), rc); 
+        uint8_t ab;
+        xQueueReceive(tcb->queue, &ab, portMAX_DELAY);
+        state = ((state << 2) | (ab & 0x3)) & 0xf;
+        static const int8_t quadratureTable[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+        tcb->value += quadratureTable[state];
+        printf("(%i) Encoder %x %x %i\n", xTaskGetTickCount(), ab, state, tcb->value);
     }
 
     // We should never reach this point
+    vQueueDelete(tcb->queue);
     vTaskDelete(NULL);
 }
 
@@ -49,7 +55,7 @@ Encoder::Encoder(gpio_num_t pinCLK, gpio_num_t pinDT) {
     // These two signals are interchangable
     pinA = pinCLK; 
     pinB = pinDT;
-    tcb = new(encoderProcess_t) { pinA, pinB };
+    tcb = new(encoderProcess_t) { pinA, pinB, NULL, 0 };
     xTaskCreate(encoderProcess, "encoder", configMINIMAL_STACK_SIZE * 4, tcb, 0, &task);
 }
 
