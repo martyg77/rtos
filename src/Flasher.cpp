3fe04ec14883ba548ac9ab2d3ededd97c26657f8
Martin Gallant <martyg@goodbit.net>
2021-04-19 17:30:27 -0700

    Flasher: Lessons learned

--- src/Flasher.cpp
+++ src/Flasher.cpp
@@ -4,32 +4,41 @@
 #include <driver/gpio.h>
 #include "Flasher.h"
 
-void flasher(void *p)
-{
-    Flasher::flasherConfig *c = (Flasher::flasherConfig *) p;
+typedef struct {
+    gpio_num_t pin;
+    int speed;
+} flasherProcess_t;
 
-    gpio_pad_select_gpio(c->pin); // PinMux magic
-    gpio_set_direction(c->pin, GPIO_MODE_OUTPUT);
-    gpio_pullup_dis(c->pin);
-    gpio_pulldown_dis(c->pin);
-    gpio_intr_disable(c->pin);
+void flasherProcess(void *p) {
+    flasherProcess_t *tcb = (flasherProcess_t *) p;
+
+    gpio_pad_select_gpio(tcb->pin); // PinMux magic
+    gpio_config_t g = {
+        .pin_bit_mask = 1ull << tcb->pin,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE};
+    gpio_config(&g);
 
     while (true)
     {
-        gpio_set_level(c->pin, 1);
-        vTaskDelay(c->speed / portTICK_PERIOD_MS);
-        gpio_set_level(c->pin, 0);
-        vTaskDelay(c->speed * 3 / portTICK_PERIOD_MS);
+        gpio_set_level(tcb->pin, 1);
+        vTaskDelay(tcb->speed / portTICK_PERIOD_MS);
+        gpio_set_level(tcb->pin, 0);
+        vTaskDelay(tcb->speed * 3 / portTICK_PERIOD_MS);
     }
 }
 
-Flasher::Flasher(gpio_num_t pin, int speed)
+Flasher::Flasher(gpio_num_t p, int s)
 {
-    this->config.pin = pin;
-    this->config.speed = speed;
-    xTaskCreate(flasher, "flasher", configMINIMAL_STACK_SIZE, (void *) &this->config, 0, &this->handle);
+    pin = p;
+    speed = s;
+    tcb = new(flasherProcess_t) { pin, speed };
+    xTaskCreate(flasherProcess, "flasher", configMINIMAL_STACK_SIZE * 4, tcb, 0, &handle);
 }
 
 Flasher::~Flasher() {
-    vTaskDelete(this->handle);
+    delete((flasherProcess_t*) tcb);
+    vTaskDelete(handle);
 }
--- src/Flasher.h
+++ src/Flasher.h
@@ -5,15 +5,12 @@
 class Flasher
 {
 public:
-    typedef struct {
-        gpio_num_t pin;
-        int speed;
-    } flasherConfig;
-
     Flasher(gpio_num_t pin, int speed);
     ~Flasher();
 
 private:
-    flasherConfig config = { GPIO_NUM_NC, 0 };
+    gpio_num_t pin = GPIO_NUM_NC;
+    int speed = 0;
     TaskHandle_t handle = NULL;
+    void* tcb = NULL;
 };
